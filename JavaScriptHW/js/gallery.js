a7f39b3f702f61505514739c2f9cf0dc2355a390
Elizaveta A Lyapina <55664188+ealyapina@users.noreply.github.com>
2024-08-27 18:44:10 +0000

    better code

--- JavaScriptHW/js/gallery.js
+++ JavaScriptHW/js/gallery.js
@@ -2,10 +2,13 @@ function upDate(previewPic){
 
     let altText = previewPic.alt;
     let source = previewPic.src;
-    document.getElementById("image").innerHTML = '<div style="height:100%;background-repeat:no-repeat;background-size:cover;background-image:url(' + "'" + source + "');" +'">'+ altText+ '</div>' ;
+    document.getElementById("image").innerHTML=altText;
+    document.getElementById("image").style.backgroundImage = "url(" + source + ")";
       }
   
   function unDo(){
       const altText = "Hover over an image below to display here.";
-     document.getElementById("image").innerHTML =altText;
+      document.getElementById("image").innerHTML =altText;
+      document.getElementById("image").style.backgroundImage = 'url("")';
+
   }
